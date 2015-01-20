ac93d5d6ecbd1ad93ceebc3372a488a058dbe5c3
Clem Freeman & Kathryn Carr <student@epicodus.com>
2015-01-20 11:22:48 -0800

    Add id to classes and spec_helper file

--- lib/task.rb
+++ lib/task.rb
@@ -1,8 +1,9 @@
 class Task
-  attr_reader(:description)
+  attr_reader(:description, :list_id)
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
+    @list_id = attributes.fetch(:list_id)
   end
 
   define_singleton_method(:all) do
@@ -10,21 +11,18 @@ class Task
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch("description")
-      tasks.push(Task.new({:description => description}))
+      list_id = task.fetch("list_id").to_i()
+      tasks.push(Task.new({:description => description, :list_id =>list_id}))
     end
     tasks
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}');")
-  end
-
-  define_singleton_method(:clear) do
-    @@all_tasks = []
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
   end
 
   define_method(:==) do |task_to_compare|
-    description == task_to_compare.description()
+    (description == task_to_compare.description()) && (list_id == task_to_compare.list_id())
   end
 
 end
